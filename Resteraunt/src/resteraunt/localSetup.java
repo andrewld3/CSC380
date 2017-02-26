f15531c5e5a0a4a185ddfd5396ea59a1f36523c8
Randy Nguyen <Rnguyen@oswego.edu>
2017-02-26 01:00:35 -0500

    Update localSetup.java

--- Resteraunt/src/resteraunt/localSetup.java
+++ Resteraunt/src/resteraunt/localSetup.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package resteraunt;
+package restaurant;
 
 import java.util.Map;
 import java.util.HashMap;
