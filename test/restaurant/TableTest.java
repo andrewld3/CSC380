142da7b57d4d5b0170ba94bdacde9e4cd69b7e56
Andrew <Andrew@129.3.218.106>
2017-03-24 23:07:33 -0400

    start test

--- test/restaurant/TableTest.java
+++ test/restaurant/TableTest.java
@@ -2,19 +2,9 @@ package restaurant;
 
 import java.util.HashMap;
 import java.util.Map;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-import org.junit.Before;
 
 public class TableTest {
-    
-    @Before
-    public void initializeTest() {
-        Restaurant test = new Restaurant();
-        Map<String, MenuItem> testMenu = new HashMap<>();
-        testMenu = test.menu;
-    }
     
     @Test
     public void testAddOrderItem() {
