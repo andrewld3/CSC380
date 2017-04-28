81c44cb630df151d884b24265b9b78565b5742a4
Andrew <Andrew@DESKTOP-TT3DKIT>
2017-04-28 15:07:44 -0400

    removed license code. We charge for this crap.

--- src/restaurant/Login.java
+++ src/restaurant/Login.java
@@ -5,11 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author RandyNguyen
